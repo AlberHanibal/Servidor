8f9afd17f7f8ed77dc6dc5eac02db9df4af40721
AlberHanibal <alberhanibal@portatil.com>
2020-01-15 17:38:58 +0100

    15/01/2020

--- socialdaw/index.php
+++ socialdaw/index.php
@@ -2,10 +2,9 @@
 require 'vendor/autoload.php';
 require 'cargarconfig.php';
 
-use NoahBuscher\Macaw\Macaw;
-use controller\PruebaController;
+use \NoahBuscher\Macaw\Macaw;
 
-echo "<pre>" . var_dump($_SERVER["REQUEST_URI"]) . var_dump($_SERVER["QUERY_STRING"]) . "</pre>";
+/* echo "<pre>" . var_dump($_SERVER["REQUEST_URI"]) . var_dump($_SERVER["QUERY_STRING"]) . "</pre>";
 Macaw::get($URL_PATH . '/', function () {
   echo 'principal';
 });
@@ -32,6 +31,22 @@ Macaw::get($URL_PATH . '/demo2/(:any)', "controller\PruebaController@foo");
 // Captura de URL no definidas.
 Macaw::error(function() {
   echo '404 :: Not Found';
-});
+}); */
+
+/*
+  rutas
+    listado pantalla principal -> /
+    registrarse -> (con el macaw get) /registro
+    recibir registro -> (con el macaw post) /registro
+    hacer login y recibir login igual en /login
+*/
+
+Macaw::get($URL_PATH . '/', "controller\RedSocialController@listado");
+
+Macaw::get($URL_PATH . '/registro', "controller\RedSocialController@registro");
+Macaw::post($URL_PATH . '/registro', "controller\RedSocialController@recibirRegistro");
+
+Macaw::get($URL_PATH . '/login', "controller\RedSocialController@login");
+Macaw::post($URL_PATH . '/login', "controller\RedSocialController@recibirLogin");
 
 Macaw::dispatch();
