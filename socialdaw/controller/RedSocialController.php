8619e72df6cd0ba30a17b9e9936049b483b09d17
AlberHanibal <alberhanibal@portatil.com>
2020-01-16 18:23:30 +0100

    version 0.1

--- socialdaw/controller/RedSocialController.php
+++ socialdaw/controller/RedSocialController.php
@@ -1,18 +1,20 @@
 <?php
 namespace controller;
 
-use \dawfony\Ti;
+use dawfony\Ti;
+use model\OrmRedSocial;
+use model\Usuario;
 
 class RedSocialController extends Controller
 {
     function listado() {
         $title = "Listado";
+        $posts = (new OrmRedSocial)->obtenerTodosLosPost();
         global $URL_PATH;
-        echo Ti::render("view/ListadoView.phtml", compact('title', 'URL_PATH'));
+        echo Ti::render("view/ListadoView.phtml", compact('title', 'URL_PATH', 'posts'));
     }
 
     function registro() {
-
         $title = "Registro";
         global $URL_PATH;
         echo Ti::render("view/RegistroView.phtml", compact('title', 'URL_PATH'));
@@ -20,23 +22,56 @@ class RedSocialController extends Controller
 
     function recibirRegistro() {
         // si comprobaciones falso -> RegistroView
-
+        $usuario = new Usuario;
+        $usuario->login = $_REQUEST["login"];
+        $usuario->password = password_hash($_REQUEST["password"], PASSWORD_DEFAULT);
+        $usuario->nombre = $_REQUEST["nombre"];
+        $usuario->email = $_REQUEST["email"];
+        $usuario->rol_id = 0;
+        (new OrmRedSocial)->insertarUsuario($usuario);
         // comprobaciones bien 
         global $URL_PATH;
         header("Location: $URL_PATH/login");
     }
 
     function login() {
+        $login = "";
+        $errorLogin = "";
+        $errorPassword = "";
         $title = "Login";
         global $URL_PATH;
-        echo Ti::render("view/LoginView.phtml", compact('title', 'URL_PATH'));
+        echo Ti::render("view/LoginView.phtml", compact('title', 'URL_PATH', 'login', 'errorLogin', 'errorPassword'));
     }
 
     function recibirLogin() {
-        // si comprobaciones falso -> LoginView
+        $login = $_REQUEST["login"];
+        $password = $_REQUEST["password"];
+        $errorLogin = "";
+        $errorPassword = "";
+        $hayErrores = false;
+        $usuario = (new OrmRedSocial)->obtenerUsuario($login);
+        if (!$usuario) {
+            $errorLogin = "Login incorrecto";
+            $login = "";
+            $hayErrores = true;
+        } else {
+            if (!password_verify($password, $usuario->password)) {
+                $errorPassword = "Contraseña incorrecta";
+                $hayErrores = true;
+            }
+        }
+        if (!$hayErrores) {
+            session_start();
+            $_SESSION["login"] = $usuario->login;
+            $_SESSION["rol"] = $usuario->rol_id;
+            var_dump($_SESSION);
+            global $URL_PATH;
+            header("Location: $URL_PATH/");
+        } else {
+            global $URL_PATH;
+            $title = "Login";
+            echo Ti::render("view/LoginView.phtml", compact('title', 'URL_PATH', 'login', 'errorLogin', 'errorPassword'));
+        }
         
-        // comprobaciones bien 
-        global $URL_PATH;
-        header("Location: $URL_PATH/");
     }
 }
--- socialdaw/model/OrmRedSocial.php
+++ socialdaw/model/OrmRedSocial.php
@@ -4,4 +4,22 @@ use dawfony\Klasto;
 
 class OrmRedSocial {
 
+    function insertarUsuario($usuario) {
+        $bd = Klasto::getInstance();
+        $sql = "INSERT INTO usuario (login, password, rol_id, nombre, email) VALUES (?, ?, ?, ?, ?)";
+        $bd->execute($sql, [$usuario->login, $usuario->password, $usuario->rol_id, $usuario->nombre, $usuario->email]);
+    }
+
+    function obtenerUsuario($login) {
+        $bd = Klasto::getInstance();
+        $sql = "SELECT login, password, rol_id, nombre, email FROM usuario WHERE login = ?";
+        return $bd->queryOne($sql, [$login], 'model\Usuario');
+    }
+
+    function obtenerTodosLosPost() {
+        $bd = Klasto::getInstance();
+        // $sql = "SELECT id, fecha, resumen, texto, foto, categoria_post_id, usuario_login, descripcion FROM post JOIN categoria_post ON categoria_post_id = id";
+        $sql = "SELECT id, fecha, resumen, texto, foto, categoria_post_id, usuario_login FROM post";
+        return $bd->query($sql, [], 'model\Post');
+    }
 }
