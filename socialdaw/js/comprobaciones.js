d4480fbf49ee0f45778a34c43e5ed7d9e3096bfa
Alberto <AlberHanibal@sobremesa.com>
2020-02-01 17:44:31 +0100

    Comprobación js y forms

--- socialdaw/js/comprobaciones.js
+++ socialdaw/js/comprobaciones.js
@@ -3,3 +3,25 @@ function confirmarLogout() {
         location.href=URL_PATH + "/cerrarSesion";
     }
 }
+
+function comprobacionRegistro() {
+    let datosBuenos = true;
+    let login = document.getElementById("login");
+    if (login.value == "") {
+        document.getElementById("errorLogin").textContent = "Login vacío";
+        datosBuenos = false;
+    }
+    let nombre = document.getElementById("nombre");
+    if (nombre.value == "") {
+        document.getElementById("errorNombre").textContent = "Nombre vacío";
+        datosBuenos = false;
+    }
+    let password = document.getElementById("password");
+    let password2 = document.getElementById("password2");
+    if (password.value != password2.value) {
+        document.getElementById("errorContrasena").textContent = "Contraseña distinta";
+        password2.value = "";
+        datosBuenos = false;
+    }
+    return datosBuenos;
+}
